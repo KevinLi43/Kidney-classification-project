641612e491d7a1d4c94bea6e2ace9a29fa719b69
KevinLi43 <1764257688@qq.com>
2024-01-13 21:35:11 -0500

    set up

--- setup.py
+++ setup.py
@@ -7,16 +7,15 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
-AUTHOR_USER_NAME = "krishnaik06"
+AUTHOR_USER_NAME = "KevinLi43"
 SRC_REPO = "cnnClassifier"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+
 
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
     description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
