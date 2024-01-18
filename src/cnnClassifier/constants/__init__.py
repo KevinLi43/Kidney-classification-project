3252c9e3eabcbb2ddb848bb960b039099b25604f
KevinLi43 <1764257688@qq.com>
2024-01-17 21:05:05 -0500

    add training

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
-CONFIG_FILE_PATH = Path("config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+# CONFIG_FILE_PATH = Path("config/config.yaml")
+CONFIG_FILE_PATH = Path("C:/Users/17642/Desktop/kidney-classification-project/config/config.yaml")
+PARAMS_FILE_PATH = Path("C:/Users/17642/Desktop/kidney-classification-project/params.yaml")
